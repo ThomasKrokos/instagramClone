be45963e1d1bbe00b60e72179b095896bf306b09
Thomas Krokos <tommyk101@yahoo.com>
2024-06-13 23:39:23 -0500

    added mui theme skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import Login from "./pages/Login";
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#554400",
+      },
+      secondary: {
+        main: "#224545",
+      },
+    },
+  });
 
   return (
     <>
-      <div>Yo</div>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Login />
+      </ThemeProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <>
+      <div>Login Page</div>
+    </>
+  );
+};
 
-export default Login
+export default Login;
