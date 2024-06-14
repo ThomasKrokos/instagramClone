48d6e37cfff2457bd47e6ee7096cebee2806fed1
Thomas Krokos <tommyk101@yahoo.com>
2024-06-14 01:13:55 -0500

    working on the login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,25 @@
+import { Box, Container, Stack, Typography } from "@mui/material";
 import React from "react";
 
 const Login = () => {
   return (
     <>
-      <div>Login Page</div>
+      <Container maxWidth="sm">
+        <Stack spacing={2} direction={"row"} alignItems={'center'} justifyContent={'center'}>
+          <Box
+            component="img"
+            alt="Phone"
+            src="../assets/loginpagephone.png"
+            />
+
+          <Stack direction={"column"}>
+            <Typography>Login</Typography>
+            <Typography>Login</Typography>
+            <Typography>Login</Typography>
+            <Typography>Login</Typography>
+          </Stack>
+        </Stack>
+      </Container>
     </>
   );
 };
